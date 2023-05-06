2f5af9e83f144d13bad2bf45830224ce662d2738
unknown <riic.nun@gmail.com>
2023-05-06 17:52:52 -0300

    Add: Adicionado upload para google Driver

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const path = require("path");
+const fsPromises = require("fs").promises;
 const fs = require("fs");
-const { GoogleAuth } = require("google-auth-library");
-const { authorize } = require("./oAuth.js");
+const { GoogleAuth, auth } = require("google-auth-library");
 const { google } = require("googleapis");
+const { authenticate } = require("@google-cloud/local-auth");
 let SftpClient = require("ssh2-sftp-client");
 const schedule = require("node-schedule");
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/docs",
+  "https://www.googleapis.com/auth/drive",
+  "https://www.googleapis.com/auth/drive.appdata",
+  "https://www.googleapis.com/auth/drive.apps.readonly",
+  "https://www.googleapis.com/auth/drive.file",
+];
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
 const dotenvPath = path.join(__dirname, ".", ".env");
 require("dotenv").config({ path: dotenvPath });
@@ -19,7 +30,7 @@ const config = {
 let dst = "./";
 const src = "/backups";
 
-async function main() {
+async function main(authClient) {
   const newDate = Date.now();
   if (!fs.existsSync(newDate.toString())) {
     //Efetua a criação do diretório
@@ -28,65 +39,57 @@ async function main() {
   dst = `./backups/${newDate}`;
 
   const client = new SftpClient("upload-test");
+
   try {
     await client.connect(config);
     client.on("download", (info) => {
-      console.log(`Listener: Download ${info.source}`);
+      // console.log(`Listener: Download ${info.source}`);
     });
     let rslt = await client.downloadDir(src, dst);
-    console.log(`Resultado ${rslt}`);
-    return rslt;
+
+    const files = fs.readdirSync(`./backups/${newDate}`);
+    const drive = google.drive({ version: "v3", auth: authClient });
+
+    try {
+      files.map(async (file) => {
+        console.log(file);
+        const uploads = await drive.files.create({
+          requestBody: {
+            name: file,
+            fields: ["id"],
+          },
+          media: {
+            mimiType: "*/*",
+            body: fs.createReadStream(`./backups/${newDate}/${file}`),
+          },
+        });
+
+        console.log(uploads);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    return "ok";
   } finally {
     client.end();
     console.log("End program");
   }
 }
 
-const job = schedule.scheduleJob("1 0 12 * * *", () => {
-  main()
-    .then((msg) => {
-      console.log(msg);
-    })
-    .catch((err) => {
-      console.log(`main error: ${err.message}`);
-    });
+const job = schedule.scheduleJob("1 0 12 * * *", async () => {
+  await authorize().then(listFiles).catch(console.error);
+  console.log("Backup finalizado com sucesso");
+});
+
+const job2 = schedule.scheduleJob("1 0 15 * * *", async () => {
+  await authorize().then(listFiles).catch(console.error);
 
   console.log("Backup finalizado com sucesso");
 });
 
-const job2 = schedule.scheduleJob("1 0 15 * * *", () => {
-  const newDate = Date.now();
-  if (!fs.existsSync(newDate.toString())) {
-    //Efetua a criação do diretório
-    fs.mkdirSync(`./backups/${newDate.toString()}`);
-  }
-  dst = `./backups/${newDate}`;
-  main()
-    .then((msg) => {
-      console.log(msg);
-    })
-    .catch((err) => {
-      console.log(`main error: ${err.message}`);
-    });
-
-  console.log("Backup finalizado com sucesso");
-});
-
-const job3 = schedule.scheduleJob("1 0 20 * * *", () => {
-  const newDate = Date.now();
-  if (!fs.existsSync(newDate.toString())) {
-    //Efetua a criação do diretório
-    fs.mkdirSync(`./backups/${newDate.toString()}`);
-  }
-  dst = `./backups/${newDate}`;
-  main()
-    .then((msg) => {
-      console.log(msg);
-    })
-    .catch((err) => {
-      console.log(`main error: ${err.message}`);
-    });
-
+const job3 = schedule.scheduleJob("1 0 20 * * *", async () => {
+  await authorize().then(listFiles).catch(console.error);
   console.log("Backup finalizado com sucesso");
 });
 
@@ -95,20 +98,58 @@ const job3 = schedule.scheduleJob("1 0 20 * * *", () => {
  * @param {OAuth2Client} authClient An authorized OAuth2 client.
  */
 async function listFiles(authClient) {
-  const drive = google.drive({ version: "v3", auth: authClient });
-
-  console.log(drive.files.create);
-  main();
-  // const files = res.data.files;
-  // if (files.length === 0) {
-  //   console.log("No files found.");
-  //   return;
-  // }
-
-  // console.log("Files:");
-  // files.map((file) => {
-  //   console.log(`${file.name} (${file.id})`);
-  // });
+  await main(authClient);
 }
 
-authorize().then(listFiles).catch(console.error);
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fsPromises.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fsPromises.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fsPromises.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
